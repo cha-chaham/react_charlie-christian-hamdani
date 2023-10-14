dc193c500e2bbf8fccfd4110bf719d41f80e9a9a
cha-chaham <charliech1056@gmail.com>
2023-10-14 23:36:52 +0800

    feat: add new API Key and add screenshot

--- 25_Installation_Open.ai_di_React/praktikum/my_app/src/pages/openai/index.jsx
+++ 25_Installation_Open.ai_di_React/praktikum/my_app/src/pages/openai/index.jsx
@@ -11,7 +11,7 @@ import Swal from "@/utils/swal";
 import { RiOpenaiFill, RiUser3Fill } from "react-icons/ri";
 import { IconContext } from "react-icons";
 
-const APIkey = "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";
+const APIkey = import.meta.env.VITE_OPENAI_API_KEY;
 const openai = new OpenAI({
   apiKey: APIkey,
   dangerouslyAllowBrowser: true,
